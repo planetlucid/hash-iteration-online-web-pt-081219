9311df88968cfeafc84bad8f5923a8f2d14909af
Ryan S <planetlucid@gmail.com>
2019-08-25 14:40:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,7 +4,11 @@
 #   "Amanda" => 27
 # }
 
-
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+def
 
 
 def(age_appropriate_birthday)
